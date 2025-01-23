425876503cac4aeb965e026f4f4985c1fb659db7
Nam Vu <contact@namvu.net>
2025-01-23 21:49:59 +0700

    feat: update styling options and enhance content configuration

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -2,7 +2,10 @@ import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
 import {routing} from './routing';
  
+import {setRequestLocale} from 'next-intl/server';
+
 export default getRequestConfig(async ({locale}) => {
+  const newLocale = await setRequestLocale(locale);
   // Validate that the incoming `locale` parameter is valid
   if (!routing.locales.includes(locale as any)) notFound();
  
